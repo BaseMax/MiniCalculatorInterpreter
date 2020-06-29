1afd2bac679468563adb0892f9fec782bf09f079
Max Base <MaxBaseCode@Gmail.Com>
2020-06-29 19:05:47 +0430

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,2 +1,13 @@
 # Max Base
 # https://github.com/BaseMax/MiniCalculatorInterpreter
+import math
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens = (
+    'NAME',
+    'NUMBER_INT', 'NUMBER_DOUBLE',
+    'PLUS','MINUS','TIMES','DIVIDE',
+    'EQUALS','COLON',
+    'LPAREN','RPAREN',
+)
